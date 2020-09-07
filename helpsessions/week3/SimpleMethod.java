664aa25785928d2b30718a40997639de48b39965
KevinAlamo <31712869+KevinAlamo@users.noreply.github.com>
2020-09-07 16:56:40 -0600

    Create SimpleMethod.java

---
+++ helpsessions/week3/SimpleMethod.java
@@ -0,0 +1,10 @@
+public class YourProgram {
+   public static int addTen(int x){
+      int res = x + 10;
+      return res; // How would you do this in 1 line?
+   }
+   public static void main(String[] args) {
+      int res = addTen(13);
+      System.out.println(res);
+   }
+}
